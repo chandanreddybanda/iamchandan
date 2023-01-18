291dd1d095de25e3611829eba69d9e7969281cd3
chandanreddybanda <chandanreddybanda@gmail.com>
2023-01-18 22:42:56 +0530

    c-5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
+import { AuthService } from './services/auth.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SigninComponent } from './signin/signin.component';
+import { environment } from '../environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyBsbOna63y2fv6WIB5c3RCW7mvpux8V0VE",
+        authDomain: "chandan-site.firebaseapp.com",
+        projectId: "chandan-site",
+        storageBucket: "chandan-site.appspot.com",
+        messagingSenderId: "12760115829",
+        appId: "1:12760115829:web:7e11f84dce9c31994f663c",
+        measurementId: "G-W5VS98XTT0"
+      }
+};
